fd970935981a2854d9ea5161a6bffe2ecc3d660f
Lucas Perez <pluqui1@gmail.com>
2018-10-14 00:08:26 +0000

    Adicionando simbolo para avançar

--- ugv/main.py
+++ ugv/main.py
@@ -7,23 +7,32 @@ import motores
 find = find_obj
 cam = camera
 
-img1 = './BaseD.jpg'
+baseA = './baseA.jpg'
+baseB = './baseB.jpg'
+baseC = './baseC.jpg'
+baseD = './baseD.jpg'
 esquerda = './esquerda.jpg'
 direita = './direita.jpg'
 chegou = './chegou.jpg'
+frente = './frente.jpg'
 
 picExt = cam.tirafoto()
 
-img1 = cv.imread(img1, 0)
+baseA = cv.imread(baseA, 0)
+baseB = cv.imread(baseB, 0)
+baseC = cv.imread(baseC, 0)
+baseD = cv.imread(baseD, 0)
 esquerda = cv.imread(esquerda, 0)
 direita = cv.imread(direita, 0)
 chegou = cv.imread(chegou, 0)
+frente = cv.imread(frente, 0)
+
 img2 = cv.cvtColor(picExt, cv.COLOR_BGR2GRAY)
 
-busca = find.main('sift', img1, img2)
-height, width = img2.shape[:2]
+busca = find.main('sift', baseC, img2) # <-------------------
 if busca[0][0] == -999:
     print("Nada encontrado")
+
     sys.exit(1) 
 else:
     print("Padrao encontrado")
@@ -48,6 +57,10 @@ while(busca[0][0] == -999):
         x = 3
         direcao = 'esquerda'
     elif (x == 3):
+        busca = find.main('sift', frente, crop_img)
+        x = 4
+        direcao = 'frente'
+    elif (x == 4):
         print('Direcao nao encontrada')
         sys.exit(1)
 
